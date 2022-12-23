abdd0c541cc5bd8ae8b4706529b594c4f722936d
Yun0401 <jen.tina098024@gmail.com>
2022-12-24 02:43:53 +0800

    更改顏色、加密方法

--- encode.js
+++ encode.js
@@ -1,38 +1,41 @@
 
-const tran_DtoP = [25, 8, 3,14,10, 0,22,16, 4, 9, 2,15,19,17,11, 5, 1,23,12, 6,20,24,13,21,18, 7];
-const tran_PtoD = [ 5,16,10, 2, 8,15,19,25, 1, 9, 4,14,18,22, 3,11, 7,13,24,12,20,23, 6,17,21, 0];
+// const tran_DtoP = [25, 8, 3,14,10, 0,22,16, 4, 9, 2,15,19,17,11, 5, 1,23,12, 6,20,24,13,21,18, 7];
+// const tran_PtoD = [ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,];
 
 function encode(msg,username){
     let encode_Message = '';
     if (username === '王祐誠 醫師'){
         for(let i=0;i<msg.length;i++){
             let ch = msg[i];
-            let n = ch.charCodeAt();//way
-            if (64 < n && n <91) {
-                n = tran_DtoP[n-65] + 65;
+            let n = ch.charCodeAt();//way 65 97
+            if (n >= 99) {
+                n = n - 2;
             }
-            else if(96<n && n<123){
-                n = tran_DtoP[n-97] + 97;
+            else if(n>=97){
+                n = n - 2 + 26;
+            }
+            else if (n >= 67){
+                n = n-2;
             }
             else if(n !== 32){
-                n = n+2;
+                n = n -2 + 26;
             }
             encode_Message += String.fromCharCode(n);
         }
     }
-    else if (username === '公用電腦'){
-        for(let i=0;i<msg.length;i++){
-            let ch = msg[i];
-            let n = ch.charCodeAt();//way
-            if (64 < n && n <91) {
-                n = tran_PtoD[n-65] + 65;
-            }
-            else if(96<n && n<123){
-                n = tran_PtoD[(n-97)] + 97;
-            }
-            encode_Message += String.fromCharCode(n);
-        }
-    }
+    // else if (username === '公用電腦'){
+    //     for(let i=0;i<msg.length;i++){
+    //         let ch = msg[i];
+    //         let n = ch.charCodeAt();//way
+    //         if (64 < n && n <91) {
+    //             n = tran_PtoD[n-65] + 65;
+    //         }
+    //         else if(96<n && n<123){
+    //             n = tran_PtoD[(n-97)] + 97;
+    //         }
+    //         encode_Message += String.fromCharCode(n);
+    //     }
+    // }
     else encode_Message = msg;
     return encode_Message
 }
